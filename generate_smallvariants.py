4cd6bccf3d725bac3c1214d4d874335e4e4c80cd
Pranav Temkar <pranavtemkar@gmail.com>
2022-12-29 10:30:13 +0530

    [SQUASH] VoltageOS Covers: Add new walls
     - Adapt to VoltageOS
     - Thanks to @Shreedhan003 , @adeveloper79 , @M3zaque , @ therealpimi , @ Romm46 , @ onestupidbitch ( Telegram ID )
    
    [@PptO07]: Fix DeprecationWarning
    
    Bug: generate_smallvariants.py:27: DeprecationWarning: ANTIALIAS is deprecated and will be removed in Pillow 10 (2023-07-01). Use Resampling.LANCZOS instead.
    img_small = img.resize(size, Image.ANTIALIAS)
    
    Test: python generate_smallvariants.py
    throws no errors or DeprecationWarning
    
    Save the wallpaper with 1/1.5 size to wallpaper_small_path to fix blurry view of small walls
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- generate_smallvariants.py
+++ generate_smallvariants.py
@@ -17,14 +17,14 @@ def generate_smallvariants(resource):
         wallpaper_small = wallpaper[:-4] + "_small.jpg"
         wallpaper_small_path = wallpapers_path + wallpaper_small
 
-        # Save the wallpaper with 1/5 size to wallpaper_small_path
+        # Save the wallpaper with 1/1.5 size to wallpaper_small_path
         with Image.open(wallpapers_path + wallpaper) as img:
-            small_width = img.width / 5
-            small_height = img.height / 5
+            small_width = img.width / 1.5
+            small_height = img.height / 1.5
 
             size = int(small_width), int(small_height)
 
-            img_small = img.resize(size, Image.ANTIALIAS)
+            img_small = img.resize(size, Image.Resampling.LANCZOS)
             img_small.convert('RGB').save(wallpaper_small_path, "JPEG")
 
 def clean(wallpapers_path):
