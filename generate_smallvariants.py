1d8a656f418ff24d485ba5d188c706aa08baf371
Pranav Temkar <pranavtemkar@gmail.com>
2023-02-02 16:23:54 +0530

    VoltageOS Covers: Add new walls
     - All credits & thanks to ( Telegram id : @ Dawn_India https://t.me/Dawn_India ) for these new wallpapers
     - Also reduce compression for small wallpapers from 1/1.5 ==> 1/1.1 to reduce blur
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- generate_smallvariants.py
+++ generate_smallvariants.py
@@ -17,10 +17,10 @@ def generate_smallvariants(resource):
         wallpaper_small = wallpaper[:-4] + "_small.jpg"
         wallpaper_small_path = wallpapers_path + wallpaper_small
 
-        # Save the wallpaper with 1/1.5 size to wallpaper_small_path
+        # Save the wallpaper with 1/1.1 size to wallpaper_small_path
         with Image.open(wallpapers_path + wallpaper) as img:
-            small_width = img.width / 1.5
-            small_height = img.height / 1.5
+            small_width = img.width / 1.1
+            small_height = img.height / 1.1
 
             size = int(small_width), int(small_height)
 
